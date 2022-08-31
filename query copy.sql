368e2bc11121845d76e8d457568e0c099c65c019
tylerc108 <103055666+tylerc108@users.noreply.github.com>
2022-08-31 19:27:42 -0400

    updating everything

--- query copy.sql
+++ query copy.sql
@@ -1,12 +1,12 @@
 -- Creating 'playlists' table
 CREATE TABLE playlists (
+    track_uri VARCHAR(40),
     playlist_name VARCHAR(20),
     followers INT,
     songs INT,
     genre_1 VARCHAR(20),
     genre_2 VARCHAR(20),
     playlist_uri VARCHAR(40),
-    track_uri VARCHAR(40),
     artist_name VARCHAR(40),
     song_name VARCHAR(40),
     popularity_score INT,
@@ -15,12 +15,16 @@ CREATE TABLE playlists (
     UNIQUE (track_uri)
 );
 
+-- Display 'playlists' table
+SELECT * FROM playlists;
+
 -- DROP TABLE for 'playlists'
 DROP TABLE playlists CASCADE;
 
 
 -- Creating 'features' table
 CREATE TABLE features (
+    track_uri VARCHAR(40),
     danceability FLOAT,
     energy FLOAT,
     key INT,
@@ -34,13 +38,12 @@ CREATE TABLE features (
     tempo FLOAT,
     type VARCHAR(20),
     id VARCHAR(40),
-    track_uri VARCHAR(40),
     track_href VARCHAR(40),
     analysis_url VARCHAR(40),
     duration_ms INT,
     time_signature INT,
-    song_name VARCHAR(40),
     artist_name VARCHAR(40),
+    song_name VARCHAR(40),
     popularity_score INT,
     PRIMARY KEY (track_uri),
     UNIQUE (track_uri)
@@ -48,46 +51,8 @@ CREATE TABLE features (
 
 --Display 'features'
 SELECT * FROM features;
--- Display 'playlists' table
-SELECT * FROM playlists;
+
 
 --DROP TABLE for 'features'
 DROP TABLE features CASCADE;
 
--- Joining tables by song uri
-SELECT playlists.Playlist,
-    playlists.Followers,
-    playlists.Songs,
-    playlists.Genre_1,
-    playlists.Genre_2,
-    playlists.URI,
-    playlists.Song_URI,
-    playlists.Artist_Name,
-    playlists.Song_Name,
-    playlists.Popularity,
-    features.danceability,
-    features.energy,
-    features.key,
-    features.loudness,
-    features.mode,
-    features.speechiness,
-    features.acousticness,
-    features.instrumentalness,
-    features.liveness,
-    features.valence,
-    features.tempo,
-    features.type,
-    features.id,
-    features.track_href,
-    features.analysis_url,
-    features.duration_ms,
-    features.time_signature,
-    features.song_name,
-    features.artist_name,
-    features.popularity_score
-INTO song_data
-FROM playlists 
-LEFT JOIN features 
-ON playlists.Song_URI = features.URI;
-
-SELECT * FROM song_data
